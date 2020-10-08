14e58de56412afb7287206eed67e3743461d80a5
Sam <sch52@phy.duke.edu>
2020-10-08 12:21:56 -0400

    Corrected what is printed to screen, search results

--- loiSearcher.py
+++ loiSearcher.py
@@ -1,6 +1,7 @@
 # Run from same folder as LOIs.pickle. Searches LOI text for search word, and
 # returns results with most occurrences of that word. Input a frontier to limit
-# search, otherwise searches all LOIs.
+# search, otherwise searches all LOIs. Probably will not work well if the search
+# term has special characters.
 #
 # Can edit nResultsToShow to change number of results shown
 # Can edit nPreviewChars to change how many characters are shown for results.
@@ -33,6 +34,7 @@ frontiers=["AF","CF","CommF","CompF","EF","IF","NF","RF","TF","UF"]
 try:
   print(color.BOLD+"\nFrontier choices: "+str(frontiers[:]))
   frontierToSearch=input("Enter fronter to search (blank for all):\n"+color.END)
+  #Strip away quotation marks if user included them in frontier name
   frontierToSearch=frontierToSearch.strip("'")
   frontierToSearch=frontierToSearch.strip('"')
 except SyntaxError:
@@ -44,7 +46,7 @@ if frontierToSearch in frontiers:
 
 #Ask user what word to search for
 try:
-  searchWord=input(color.BOLD+"Enter word or phrase to search (case-insenstive):\n"+color.END)
+  searchWord=input(color.BOLD+"Enter word or phrase to search:\n"+color.END)
 except SyntaxError:
   pass
 
@@ -61,11 +63,11 @@ for loi in loiList:
 #Sort by number of occurrences of the search word
 matchingLOIs.sort(key = lambda x: x[3])
 matchingLOIs.reverse()
-print(color.BOLD+"\nFound "+str(len(matchingLOIs))+" matching LOIs"+color.END)
 
 print("\n====================")
 print("===SEARCH RESULTS===")
-print("====================\n")
+print("====================")
+print(color.BOLD+"Found "+str(len(matchingLOIs))+" matching LOIs\n"+color.END)
 
 #Update results to display if fewer than nResultsToShow
 if len(matchingLOIs)<nResultsToShow:
@@ -79,7 +81,9 @@ for i in range(0,nResultsToShow):
   else:
     charsToDisplay=nPreviewChars
     
-  #Print the Frontier - Filename
+  #Print:
+  # Search ranking #. Frontier - Filename
+  # Text preview
   print(color.BOLD +str(i+1)+". "+matchingLOIs[i][0]+" - "+matchingLOIs[i][1] + color.END)
   print(matchingLOIs[i][2][0:charsToDisplay]+"\n")
 f.close()
